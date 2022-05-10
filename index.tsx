48f47ce14c4ce83ed20906c297bac6b6e361a8e5
SHIBAINUNI <94232115+SHIBAINUNI@users.noreply.github.com>
2022-05-10 01:34:20 -0600

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,76 +1,23 @@
-import '@reach/dialog/styles.css'
-import 'inter-ui'
-import 'polyfills'
-import 'components/analytics'
+import Swap, { SwapProps } from './components/Swap'
+import Widget, { WidgetProps } from './components/Widget'
 
-import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
-import { MulticallUpdater } from 'lib/state/multicall'
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-import { createWeb3ReactRoot, Web3ReactProvider } from 'web3-react-core'
+export type { ErrorHandler } from './components/Error/ErrorBoundary'
+export type { FeeOptions } from './hooks/swap/useSyncConvenienceFee'
+export type { DefaultAddress, TokenDefaults } from './hooks/swap/useSyncTokenDefaults'
+export type { Theme } from './theme'
+export { darkTheme, lightTheme } from './theme'
+export type { Provider as EthersProvider } from '@ethersproject/abstract-provider'
+export type { TokenInfo } from '@uniswap/token-lists'
+export type { Provider as Eip1193Provider } from '@web3-react/types'
+export type { SupportedLocale } from 'constants/locales'
+export { SUPPORTED_LOCALES } from 'constants/locales'
 
-import Blocklist from './components/Blocklist'
-import { NetworkContextName } from './constants/misc'
-import { LanguageProvider } from './i18n'
-import App from './pages/App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import store from './state'
-import ApplicationUpdater from './state/application/updater'
-import ListsUpdater from './state/lists/updater'
-import LogsUpdater from './state/logs/updater'
-import TransactionUpdater from './state/transactions/updater'
-import UserUpdater from './state/user/updater'
-import ThemeProvider, { ThemedGlobalStyle } from './theme'
-import RadialGradientByChainUpdater from './theme/RadialGradientByChainUpdater'
-import getLibrary from './utils/getLibrary'
+export type SwapWidgetProps = SwapProps & WidgetProps
 
-const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
-
-if (!!window.ethereum) {
-  window.ethereum.autoRefreshOnNetworkChange = false
-}
-
-function Updaters() {
+export function SwapWidget(props: SwapWidgetProps) {
   return (
-    <>
-      <RadialGradientByChainUpdater />
-      <ListsUpdater />
-      <UserUpdater />
-      <ApplicationUpdater />
-      <TransactionUpdater />
-      <MulticallUpdater />
-      <LogsUpdater />
-    </>
+    <Widget {...props}>
+      <Swap {...props} />
+    </Widget>
   )
-}
-
-ReactDOM.render(
-  <StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <LanguageProvider>
-          <Web3ReactProvider getLibrary={getLibrary}>
-            <Web3ProviderNetwork getLibrary={getLibrary}>
-              <Blocklist>
-                <BlockNumberProvider>
-                  <Updaters />
-                  <ThemeProvider>
-                    <ThemedGlobalStyle />
-                    <App />
-                  </ThemeProvider>
-                </BlockNumberProvider>
-              </Blocklist>
-            </Web3ProviderNetwork>
-          </Web3ReactProvider>
-        </LanguageProvider>
-      </HashRouter>
-    </Provider>
-  </StrictMode>,
-  document.getElementById('root')
-)
-
-if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
-  serviceWorkerRegistration.register()
 }
